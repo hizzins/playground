ca7914bed5ee8e0f459c31e65a33843d2f739e3c
신희 <hizzin413@gmail.com>
2019-03-02 20:37:42 +0900

    인덱스 구조 변경

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
-export { default as ListPage } from './ListPage';
-export { default as CarouselPage } from './CarouselPage';
-export { default as FileUploaderPage } from './FileUploaderPage';
-export { default as RichEditorPage } from './RichEditorPage';
-export { default as YoutubePage } from './YoutubePage';
-export { default as NotFoundPage } from './NotFoundPage';
+export { default as ListPage } from './ListPage/ListPage';
+export { default as CarouselPage } from './CarouselPage/CarouselPage';
+export { default as FileUploaderPage } from './FileUploaderPage/FileUploaderPage';
+export { default as RichEditorPage } from './RichEditorPage/RichEditorPage';
+export { default as YoutubePage } from './YoutubePage/YoutubePage';
+export { default as NotFoundPage } from './NotFoundPage/NotFoundPage';
