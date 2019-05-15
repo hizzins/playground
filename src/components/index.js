a4fe76cc36265dbd90d3b7d7259868aebfb2b6bc
신희진 <hizzin413@gmail.com>
2019-05-15 18:09:33 +0900

    버그수정

--- src/components/index.js
+++ src/components/index.js
@@ -3,10 +3,12 @@ export { default as FileUploader} from './ui/FileUploader/FileUploader';
 export { default as ImageUploader} from './ui/ImageUploader/ImageUploader';
 export { default as Slide} from './ui/Slide/Slide';
 export { default as QuillEditor} from './ui/QuillEditor/QuillEditor';
+export { default as Toast} from './ui/Toast/Toast';
 export { default as ToastControl} from './ui/Toast/ToastControl';
 
 export { default as Counter} from './practice/Counter';
 export { default as Todos} from './practice/Todos/Todos';
 export { default as YoutubeControl} from './practice/YoutubeControl/YoutubeControl';
+export { default as YoutubeControlPlayer} from './practice/YoutubeControl/YoutubeControlPlayer';
 
 export { default as WebCamera} from './webrtc/WebCamera/WebCamera';
